4fa5cb2a61d19e06af442e37f04670d17ae295e2
airKing05 <anil050797@gmail.com>
2024-05-08 19:47:24 +0530

    templates and options api

--- server/v1/message/message.controller.js
+++ server/v1/message/message.controller.js
@@ -1,7 +1,205 @@
 
 // const Schedule =  require('../../../modules/schedule.schema');
 const { ObjectId } = require('mongodb');
+// const { templatesData } = require('./messageTemplatesStaticData');
+// const { templatesData }
 
+const templatesData = [
+    {
+        subjectId: 'aced',
+        category: 'Leave application',
+        template: `<p>
+                      Hello sir my name is
+                      <input 
+                         id='applicantName'
+                         type='text' 
+                         placeholder='Name'
+                       />
+                      &
+                      <span>
+                          <input
+                              type='text'
+                              placeholder='Gender'
+                              class='tooltip__input'
+                              value = value
+                          />
+                      </span>
+
+                      i want to take leave from
+                      <span>
+                        <input 
+                         id='applicationStartDate'
+                         type='date' 
+                         placeholder='Date'
+                       />
+                      </span>
+                       to
+                      <span>
+                        <input
+                         id='applicationEndDate'
+                         type='date'
+                         placeholder='Date'
+                       />
+                      </span>
+                       the reason behind this is  ths
+                      <span>
+                       <select class="custom_selector__send_message__select" id="select_for" name="reason">
+                         <option class="option-style" value="medical">Medical</option>
+                         <option  class="option-style" value="festival">Festival</option>
+                         <option  class="option-style" value="work">Work</option>
+                         <option  class="option-style" value="tour">Tour</option>
+                        </select>
+                      </span>
+                  </p>`
+    },
+    {
+        subjectId: 'aced1',
+        category: 'Emergency meeting',
+        template: `<p>
+                      Hello Teams
+                      <input 
+                         id='applicantName'
+                         type='text' 
+                         placeholder='Name'
+                       />
+                      this message is regarding to the a meeting on
+                      <span>
+                        <input 
+                         id='applicationStartDate'
+                         type='date' 
+                         placeholder='Date'
+                       />
+                      </span>
+                     
+                       the reason behind this is  ths
+                      <span>
+                       <select class="custom_selector__send_message__select" id="select_for" name="reason">
+                         <option class="option-style" value="project-discussion">Project discussion</option>
+                         <option  class="option-style" value="hiring">Hiring</option>
+                         <option  class="option-style" value="work">Work</option>
+                        </select>
+                      </span>
+                  </p>`
+    },
+    {
+        subjectId: 'aced3',
+        category: 'Tour plan',
+        template: `<p>
+                      Hello Team
+                      <input 
+                         id='applicantName'
+                         type='text' 
+                         placeholder='Name'
+                       />
+                      this message is regarding to the company is planing the trip from 
+                      <span>
+                        <input 
+                         id='applicationStartDate'
+                         type='date' 
+                         placeholder='Date'
+                       />
+                      </span>
+                      to 
+                      <span>
+                        <input
+                         id='applicationEndDate'
+                         type='date'
+                         placeholder='Date'
+                       />
+                      </span>
+                     
+                       these are the option available for you guys to select anyone from that
+                       <select class="custom_selector__send_message__select" id="select_for" name="reason">
+                         <option class="option-style" value="jaipur">Jaipur</option>
+                         <option  class="option-style" value="shimla">Shimla</option>
+                         <option  class="option-style" value="tasken">Tasken</option>
+                        </select>
+                  </p>`
+    }
+];
+
+const selectionOptions = [
+    {
+        category: 'Leave application',
+        reason: [
+            {
+                label: 'Festival',
+                value: 'festival'
+            },
+            {
+                label: 'Work',
+                value: 'work'
+            },
+            {
+                label: 'Tour',
+                value: 'tour'
+            }
+        ]  
+    },
+    {
+        category: 'Tour plan',
+        cities: [
+            {
+                label: 'Jaipur',
+                value: 'jaipur'
+            },
+            {
+                label: 'Shimla',
+                value: 'shimla'
+            },
+            {
+                label: 'Tasken',
+                value: 'tasken'
+            }
+        ],
+        typesOfTrip: [
+            {
+                label: 'Business',
+                value: 'business'
+            },
+            {
+                label: 'Fun',
+                value: 'fun'
+            },
+            {
+                label: 'Yearly tour',
+                value: 'yearly-tour'
+            }
+        ]
+    },
+    {
+        category: 'Emergency meeting',
+        meetingReason: [
+            {
+                label: 'Work',
+                value: 'work'
+            },
+            {
+                label: 'Project discussion',
+                value: 'project-discussion'
+            },
+            {
+                label: 'Hiring',
+                value: 'hiring'
+            }
+        ],
+        typesOfTrip: [
+            {
+                label: 'Business',
+                value: 'business'
+            },
+            {
+                label: 'Fun',
+                value: 'fun'
+            },
+            {
+                label: 'Yearly tour',
+                value: 'yearly-tour'
+            }
+        ]
+    },
+
+]
 
 async function searchAutoCompleteSubject(req, res) {
     try {
@@ -92,8 +290,85 @@ async function addMessage(req, res) {
 };
 
 
+async function messageTemplates(req, res){
+    try {
+        const queryString = req.query;
+     
+        if (!queryString.subjectTitle) {
+            return res.status(400).json({
+                "meta": {
+                    "success": false,
+                    "message": `templated can be get by the subjectTitle`
+                },
+                "data": {},
+            })
+        }
+       
+
+        const selectedTemplates = templatesData.find((_template) => _template.category === queryString.subjectTitle)
+        if (selectedTemplates) {
+            return res.status(201).json({
+                "meta": {
+                    "success": true,
+                    "message": `Subjects successfully filtered from the Database`
+                },
+                "data": selectedTemplates,
+            })
+        }
+
+    } catch (error) {
+        return res.status(500).json({
+            "meta": {
+                "success": false,
+                "message": `Internal server error ${error}`
+            },
+            "data": null,
+        })
+        console.log("Error", error)
+    }
+}
+
+async function messageSelectedTemplateSelectionOptions(req, res){
+    try {
+        const queryString = req.query;
+
+        if (!queryString.subjectTitle) {
+            return res.status(400).json({
+                "meta": {
+                    "success": false,
+                    "message": `Options can be get by the subjectTitle`
+                },
+                "data": {},
+            })
+        }
+
+
+        const selectedOption = selectionOptions.find((_option) => _option.category === queryString.subjectTitle)
+        if (selectedTemplates) {
+            return res.status(201).json({
+                "meta": {
+                    "success": true,
+                    "message": `Options successfully filtered from the Database`
+                },
+                "data": selectedTemplates,
+            })
+        }
+
+    } catch (error) {
+        return res.status(500).json({
+            "meta": {
+                "success": false,
+                "message": `Internal server error ${error}`
+            },
+            "data": null,
+        })
+        console.log("Error", error)
+    }
+}
 
 module.exports = {
     searchAutoCompleteSubject,
     addMessage,
+    messageTemplates,
+    messageSelectedTemplateSelectionOptions
 }
--- server/v1/message/message.route.js
+++ server/v1/message/message.route.js
@@ -6,6 +6,9 @@ const messageController = require('./message.controller');
 
 router.route('/').get(AuthMiddleware.authenticate, messageController.searchAutoCompleteSubject);
 router.route('/').post(AuthMiddleware.authenticate, messageController.addMessage);
+router.route('/templates').get(AuthMiddleware.authenticate, messageController.messageTemplates);
+router.route('/templates-selection-options').get(AuthMiddleware.authenticate, messageController.messageSelectedTemplateSelectionOptions);
+
 
 
 module.exports = router;
